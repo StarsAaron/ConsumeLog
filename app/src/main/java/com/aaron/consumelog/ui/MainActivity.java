c5adeb67809a6f7ed026cd270dcfe6bee483ec91
Aaron <103514303@qq.com>
2017-09-07 12:45:41 +0800

    修改按钮样式

--- app/src/main/java/com/aaron/consumelog/ui/MainActivity.java
+++ app/src/main/java/com/aaron/consumelog/ui/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -68,9 +69,9 @@ public class MainActivity extends AppCompatActivity {
     @InjectView(R.id.columnChartView)
     ColumnChartView columnChartView;
     @InjectView(R.id.btn_pre_week)
-    Button btnPreWeek;
+    ImageView btnPreWeek;
     @InjectView(R.id.btn_next_week)
-    Button btnNextWeek;
+    ImageView btnNextWeek;
     @InjectView(R.id.pieChartView)
     PieChartView pieChartView;
     @InjectView(R.id.tv_null)
